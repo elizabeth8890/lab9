1272ee2d4c7dc5548695073a71ab3afcf6500d50
elizabeth8890 <elizabeth8890@gmail.com>
2024-07-25 17:05:48 -0400

    main created

---
+++ main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 24 15:26:04 2024
+
+@author: elizabethkallman
+"""
+
+
+print("Menu")
+print("-------------")
+print("1. Encode")
+print("2. Decode")
+print("3. Quit")
+
+my_option = int(input(" Please enter an option: "))
+
+if my_option == 1:
+    pass
+elif my_option == 2:
+    pass
+
+        
