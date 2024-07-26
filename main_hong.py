6ca144f29f8f2afd562a338d30b1abe656400aac
elizabeth8890 <elizabeth8890@gmail.com>
2024-07-26 13:49:43 -0400

    Commiting.

--- main_hong.py
+++ main_hong.py
@@ -3,40 +3,45 @@
 """
 Created on Wed Jul 24 15:26:04 2024
 
+Partner -- Hong
+
 @author: elizabethkallman
 """
+
+
 def encode(password):
-    list_password = list(password) 
+    list_password = list(password)
     password2 = []
     for i in range(len(list_password)):
         i_tmp = str(int(list_password[i]) + 3)
         password2.append(i_tmp)
         x = "".join(password2)
-    
-    return(x)
+
+    return (x)
+
 
 def decode(password):
     pass
 
+
 def main():
-    
     while True:
-        print("Menu")
+        print("\nMenu")
         print("-------------")
         print("1. Encode")
         print("2. Decode")
-        print("3. Quit")  
-        my_option = input("\nPlease enter an option: ")
+        print("3. Quit")
+        my_option = int(input("\nPlease enter an option: "))
         if my_option == 1:
             password = input("Please enter the password to encode: ")
             encoded_password = encode(password)
             print("Your password has been encoded and stored!")
         elif my_option == 2:
-            pass
+            decoded_password = decode(encoded_password)
+            print(f"The encoded password is {encoded_password} and the original password is {decoded_password}.")
         elif my_option == 3:
             break
 
+
 if __name__ == "__main__":
     main()
-
-        
